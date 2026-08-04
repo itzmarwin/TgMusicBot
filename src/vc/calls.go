492a9d23f3249d9e5ef2be21d1c0f0214a461ed5
itzmarwin <itzm4rw1n@gmail.com>
2026-08-04 17:57:38 +0530

    Update calls.go

--- src/vc/calls.go
+++ src/vc/calls.go
@@ -130,7 +130,7 @@ func (c *TelegramCalls) playSong(bot *td.Client, chatID int64, song *utils.Cache
 	)
 
 	_, err = reply.EditText(bot, text, &td.EditTextMessageOpts{
-		ReplyMarkup:           core.ControlButtons("play"),
+		ReplyMarkup:           core.ControlButtons(bot, "play"),
 		ParseMode:             "HTML",
 		DisableWebPagePreview: true,
 	})
